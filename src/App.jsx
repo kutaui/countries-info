795c9276b1595b8fbe37e7df2795f6ff0ca322d1
kutaui <kutaybekleric@gmail.com>
2023-03-19 23:01:16 +0300

    finishing searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 import { Header } from './Header/Header'
 import { Card } from './UI/Card'
+import { Searchbar } from './UI/Searchbar'
 
 function App() {
   return <>
     <Header />
-
+    <Searchbar />
   </>
 }
 
--- src/UI/Searchbar.jsx
+++ src/UI/Searchbar.jsx
@@ -1,1 +1,10 @@
-export const Searchbar = () => { }
+import styles from "../../css/Searchbar.module.css"
+
+export const Searchbar = () => {
+    return <>
+        <div className={styles.container}>
+            <img src="src/assets/white search.png" alt="" />
+            <input type="search" placeholder="Search for a country" />
+        </div>
+    </>
+}
