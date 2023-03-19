4c587005504af2182bd9bf9394dd92b0e2f9de4a
kutaui <kutaybekleric@gmail.com>
2023-03-19 18:38:30 +0300

    header finished

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import styles from "../../dist/Header.module.css"
+export const Header = () => {
+    return <>
+        <header className={styles.header}>
+            <h2>Where in the world?</h2>
+            <div className={styles.theme}>
+                <img src="src/assets/theme-dark.png" alt="" />
+                <h5>Dark Mode</h5>
+
+            </div>
+
+
+        </header>
+    </>
+}
